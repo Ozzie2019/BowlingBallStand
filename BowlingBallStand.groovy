8d5d2ead80b41c0c65209adb1cc844da7669ed2a
CAD User <CAD User@VM-898623>
2023-09-23 14:56:18 -0400

    Creating workflow

--- BowlingBallStand.groovy
+++ BowlingBallStand.groovy
@@ -1,1 +1,7 @@
-// code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Toroid
+import eu.mihosoft.vrl.v3d.Cylinder
+baseOg = new Toroid(50, 75, 90, 90).toCSG() 
+base = baseOg.rotx(90)
+.movez(26)
+return [base,]
