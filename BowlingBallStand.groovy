5baeb1eac05608ea1d4e105d38289f831ddc6fd6
CAD User <CAD User@VM-898623>
2023-09-23 15:09:59 -0400

    i cut off the bottom so it can be flat

--- BowlingBallStand.groovy
+++ BowlingBallStand.groovy
@@ -4,4 +4,11 @@ import eu.mihosoft.vrl.v3d.Cylinder
 baseOg = new Toroid(50, 75, 90, 90).toCSG() 
 base = baseOg.rotx(90)
 .movez(26)
-return [base,]
+
+base = base.difference(
+	base.getBoundingBox()
+	.toZMax()
+	.movez(5)
+	)
+	.toZMin()
+return [base]
